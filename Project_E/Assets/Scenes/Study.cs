1525282b2870fc6ad951d3c15f7113734c53128d
CHO KYEONGSEOK <rudtjr9522@gmail.com>
2025-11-26 16:57:59 +0900

    <docs>Week10 Day1

--- Project_E/Assets/Scenes/Study.cs
+++ Project_E/Assets/Scenes/Study.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class Study : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Hello World.");
     }
 
     // Update is called once per frame
