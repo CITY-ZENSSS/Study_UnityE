f00aa4c914eb31a7f710dfd0c6558c74578bec32
CHO KYEONGSEOK <rudtjr9522@gmail.com>
2025-12-04 01:01:54 +0900

    <Docs> Week10 Day6

--- Project_E/Assets/Script/Popup.cs
+++ Project_E/Assets/Script/Popup.cs
@@ -6,15 +6,16 @@ public class Popup : MonoBehaviour
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI contentText;
 
-    public void SetContent(string title, string content)
+    public void SetContent(string title, string content) // ÆË¾÷ ³»¿ë ¼³Á¤
     {
         titleText.text = title;
         contentText.text = content;
     }
 
 
-    public void ClosePopup()
+    public void ClosePopup() // ÆË¾÷ ´Ý±â
     {
         Destroy(gameObject);
     }
 }
+// 
